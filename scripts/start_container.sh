13d8ff719516d0fc9fce06bf1c5b1d54ab08d890
Janne Pellikka <janne@pellikka.me>
2016-04-28 10:59:08 +0300

    fixed typo

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v $(pwd):/home/foobar/work/ -p 8888 peltikalle/analytics-playground
+docker run --rm -it -v $(pwd):/home/foobar/work/ -p 8888:8888 peltikalle/analytics-playground
